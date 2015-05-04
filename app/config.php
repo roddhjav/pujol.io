9ca125dd55fac0ff959974f8865931449a9b599f
Alexandre PUJOL <alexandre.pujol.1@etu.univ-amu.fr>
2015-05-04 12:41:49 +0100

    Add generic config file

---
+++ app/config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Error Reporting
+ */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+/**
+ * Configuration for: Project URL
+ */
+define('URL', '<URL>');
+
+
+/**
+ * Mail adress
+ */
+define('MAIL', '<MAIL>');
+
+
+/**
+ * Configuration for Database
+ */
+define('DB_HOST', '<hostname>');
+define('DB_NAME', '<dbname>');
+define('DB_USER', '<username>');
+define('DB_PASS', '<password>');
+
+
+/**
+ * Table(s) name
+ */
+$db_visits='visits';
+?>
+
