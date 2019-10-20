2c9ab7c85e3444fcc69f37fca5ea1e44bcec356c
Alexandre Pujol <alexandre@pujol.io>
2019-10-20 14:14:48 +0100

    Gitlab page supports HTTPS redirection, so remove the javascript.

--- themes/sustain/static/js/main.js
+++
@@ -1,5 +0,0 @@
-// JavaScript-based HTTPS redirection because we're running on GitLab's servers.
-var host = "pujol.io";
-if ((host == window.location.host) && (window.location.protocol != 'https:')) {
-  window.location = window.location.toString().replace(/^http:/, "https:");
-}
